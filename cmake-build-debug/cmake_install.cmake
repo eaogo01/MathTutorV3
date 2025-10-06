af8c7983b21b1675b8d3db5ac833488e6ec6e1bf
auhill01 <auhill01@wsc.edu>
2025-10-06 09:26:42 -0500

    a bracket was in 108 i removed it and tested the code

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/eaogo01/OneDrive - Wayne State College/Documents/CSC 150/untitled3
+# Install script for directory: C:/Users/auhill01/OneDrive - Wayne State College/C++/MathTutorV1/MathTutorV3
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/eaogo01/OneDrive - Wayne State College/Documents/CSC 150/untitled3/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/auhill01/OneDrive - Wayne State College/C++/MathTutorV1/MathTutorV3/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
